16fc81312c33f902fa7fbecb8694f43c168782bd
Anita Holm <anita@anitaholm.blog>
2025-05-04 14:07:50 +0300

    Handle command lines arguments

--- script.py
+++ script.py
@@ -1,3 +1,19 @@
+import os
+import sys
+
+def main(input_image_path, output_path):
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    base_name = os.path.splitext(os.path.basename(input_image_path))[0]
+    print(base_name)
 
 if __name__ == "__main__":
-    print("Hello World!")
+    if len(sys.argv) != 3:
+        print("Usage: python script.py <input_image_path> <output_folder>")
+        sys.exit(1)
+
+input_image_path = sys.argv[1]
+output_path = sys.argv[2]
+main(input_image_path, output_path)
+    
