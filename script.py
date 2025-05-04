9467c93cba6c1204aaa458c41f9f7aef6ce4d92a
Anita Holm <anita@anitaholm.blog>
2025-05-04 14:48:35 +0300

    First version of image resize

--- script.py
+++ script.py
@@ -1,12 +1,23 @@
 import os
 import sys
+from PIL import Image
+
+def resize_and_save(image, width, height, dpi, output_path, base_name, format):
+    size_pixels = (int(width*dpi), int(height*dpi))
+    resized = image.copy()
+    resized = resized.resize(size_pixels, Image.LANCZOS)
+    file_path = os.path.join(output_path, base_name + "_" + str(width) + "x" + str(height) + "." + format)
+    resized.save(file_path, format=format.upper(), dpi=(dpi,dpi))
+    print("Saved:", file_path)
 
 def main(input_image_path, output_path):
     if not os.path.exists(output_path):
         os.makedirs(output_path)
 
     base_name = os.path.splitext(os.path.basename(input_image_path))[0]
-    print(base_name)
+    image = Image.open(input_image_path)
+
+    resize_and_save(image, 20, 30, 300, output_path, base_name, "png")
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
