98b9edd57119fc1b9e92acf2ba054e72fb06bfd4
Anita Holm <anita@anitaholm.blog>
2025-05-04 15:50:09 +0300

    Resize to different ratios, png and jpg

--- script.py
+++ script.py
@@ -2,12 +2,27 @@ import os
 import sys
 from PIL import Image
 
+def size_to_str(width, height):
+    ISO_sizes = {
+        (33.1,46.8): "A0",
+        (23.4,33.1): "A1", 
+        (16.5,23.4): "A2", 
+        (11.7,16.5): "A3", 
+        (8.3,11.7): "A4", 
+        (5.8,8.3): "A5",
+    }
+    if (width,height) in ISO_sizes:
+        return ISO_sizes[(width,height)]
+    else:
+        return f"{width}x{height}"
+
 def resize_and_save(image, width, height, dpi, output_path, base_name, format):
     size_pixels = (int(width*dpi), int(height*dpi))
     resized = image.copy()
     resized = resized.resize(size_pixels, Image.LANCZOS)
-    file_path = os.path.join(output_path, base_name + "_" + str(width) + "x" + str(height) + "." + format)
-    resized.save(file_path, format=format.upper(), dpi=(dpi,dpi))
+    size_str = size_to_str(width, height)
+    file_path = os.path.join(output_path, f"{base_name}_{size_str}.{format}")
+    resized.save(file_path, dpi=(dpi,dpi))
     print("Saved:", file_path)
 
 def main(input_image_path, output_path):
@@ -17,7 +32,19 @@ def main(input_image_path, output_path):
     base_name = os.path.splitext(os.path.basename(input_image_path))[0]
     image = Image.open(input_image_path)
 
-    resize_and_save(image, 20, 30, 300, output_path, base_name, "png")
+    dpi = 300
+
+    sizes = {
+        "2:3": [(20,30), (16,24), (12,18), (8,12), (4,6)],
+        "4:5": [(16,20), (8,10), (4,5)],
+        "ISO": [(33.1,46.8), (23.4,33.1), (16.5,23.4), (11.7,16.5), (8.3,11.7), (5.8,8.3)],
+        "11x4": [(11,14)],
+    }
+
+    for ratio, size_list in sizes.items():
+        for size in size_list:
+            for fmt in ["png", "jpg"]:
+                resize_and_save(image, size[0], size[1], dpi, output_path, base_name, fmt)
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
